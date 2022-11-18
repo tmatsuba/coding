f9379e386e89472bf3af4330869752087e612f9b
tmatsuba <tma@gside.org>
2022-11-18 09:15:05 +0900

    abc276 a

---
+++ _result/_abc276/a/main.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"os"
+	"bufio"
+	"fmt"
+	"strconv"
+	"math"
+	"math/bits"
+)
+
+func main() {
+	// buf := make([]byte, 1024*1024)
+	// sc.Buffer(buf, bufio.MaxScanTokenSize)
+	sc.Split(bufio.ScanWords)
+
+	N := nextInt()
+	M := nextInt()
+	A := make([]int, N)
+	m := map[int]bool
+
+	for i:=0; i<N; i++ {
+		A[i] = nextInt()
+		m[i] = true
+	}
+
+	for _,a := range A {
+		c1 := a
+		c2 := (a+1) % M
+
+	}
+
+}
+
+func recur(
+
+
+var sc = bufio.NewScanner(os.Stdin)
+
+func nextLine() string {
+    sc.Scan()
+    return sc.Text()
+}
+
+func nextInt() int {
+    sc.Scan()
+    i, e := strconv.Atoi(sc.Text())
+    if e != nil {
+        panic(e)
+    }
+    return i
+}
+
+func Max(x, y int) int {
+    if x < y {
+        return y
+    }
+    return x
+}
+
+func Min(x, y int) int {
+    if x > y {
+        return y
+    }
+    return x
+}
+
+func Abs(x int) int {
+	return int(math.Abs(float64(x)))
+}
+
+func atoi(s string) int {
+	ret, e := strconv.Atoi(s)
+	if e != nil {
+		panic(e)
+	}
+	return ret
+}
+
+func pow(a, b int) int {
+	return int(math.Pow(float64(a), float64(b)))
+}
+
+func combination(set []string, n int) (subsets [][]string) {
+	length := uint(len(set))
+
+	if n > len(set) {
+		n = len(set)
+	}
+
+	for subsetBits := 1; subsetBits < (1 << length); subsetBits++ {
+		if n > 0 && bits.OnesCount(uint(subsetBits)) != n {
+			continue
+		}
+
+		var subset []string
+
+		for object := uint(0); object < length; object++ {
+			if (subsetBits>>object)&1 == 1 {
+				subset = append(subset, set[object])
+			}
+		}
+		subsets = append(subsets, subset)
+	}
+	return subsets
+}
