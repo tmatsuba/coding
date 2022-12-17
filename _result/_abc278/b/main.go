55f2bc49438875ccbfc30812ab8dd56a7f53a34d
tmatsuba <tma@gside.org>
2022-12-17 17:39:46 +0900

    abc278 b

---
+++ _result/_abc278/b/main.go
@@ -0,0 +1,133 @@
+package main
+
+import (
+	"os"
+	"bufio"
+	"fmt"
+	"strconv"
+	"math"
+	"math/bits"
+	"strings"
+)
+
+func main() {
+	// buf := make([]byte, 1024*1024)
+	// sc.Buffer(buf, bufio.MaxScanTokenSize)
+	sc.Split(bufio.ScanWords)
+
+	H := nextInt()
+	M := nextInt()
+    var A, B, C, D int
+	A = int(H/10)
+	B = H-A*10
+	C = int(M/10)
+	D = M-C*10
+
+
+	if A > 1 && C > 3 {
+
+		if A == 2 && B == 3 {
+			A = 0
+			B = 0
+		} else if B == 9 {
+			A++
+			B++
+		} else {
+			B++
+		}
+		C = 0
+		D = 0
+	}
+
+	if B > 5 {
+		if A == 2 {
+			A = 0
+		} else {
+			A ++
+		}
+		B = 0
+		C = 0
+		D = 0
+	} 
+
+
+	H = A * 10 + B
+	M = C * 10 + D
+	fmt.Println(H,  M)	
+
+}
+
+
+var sc = bufio.NewScanner(os.Stdin)
+
+func nextLine() string {
+    sc.Scan()
+    return sc.Text()
+}
+
+func nextInt() int {
+    sc.Scan()
+    i, e := strconv.Atoi(sc.Text())
+    if e != nil {
+        panic(e)
+    }
+    return i
+}
+
+func Max(x, y int) int {
+    if x < y {
+        return y
+    }
+    return x
+}
+
+func Min(x, y int) int {
+    if x > y {
+        return y
+    }
+    return x
+}
+
+func Abs(x int) int {
+	return int(math.Abs(float64(x)))
+}
+
+func atoi(s string) int {
+	ret, e := strconv.Atoi(s)
+	if e != nil {
+		panic(e)
+	}
+	return ret
+}
+
+func pow(a, b int) int {
+	return int(math.Pow(float64(a), float64(b)))
+}
+
+func combination(set []string, n int) (subsets [][]string) {
+	length := uint(len(set))
+
+	if n > len(set) {
+		n = len(set)
+	}
+
+	for subsetBits := 1; subsetBits < (1 << length); subsetBits++ {
+		if n > 0 && bits.OnesCount(uint(subsetBits)) != n {
+			continue
+		}
+
+		var subset []string
+
+		for object := uint(0); object < length; object++ {
+			if (subsetBits>>object)&1 == 1 {
+				subset = append(subset, set[object])
+			}
+		}
+		subsets = append(subsets, subset)
+	}
+	return subsets
+}
+
+func ArrayJoin(a []int, delim string) string {
+	return strings.Trim(strings.Replace(fmt.Sprint(a), " ", delim, -1), "[]")
+}
