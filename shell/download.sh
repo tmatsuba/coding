f36e80af120c48cbd5305ced58cde62b02abc3d3
tmatsuba <tma@gside.org>
2023-01-05 17:54:07 +0900

    modify for tessoku_book.

--- shell/download.sh
+++ shell/download.sh
@@ -4,6 +4,13 @@ echo -e CONTEST: $dir
 echo -n QUESTION?
 read q
 rm -rf test
-echo http://$dir.contest.atcoder.jp/tasks/$dir"_"$q
-oj download http://$dir.contest.atcoder.jp/tasks/$dir"_"$q
+
+if [ ${dir} == "tessoku_book" ]; then
+    echo https://atcoder.jp/contests/tessoku-book/tasks/tessoku_book_$q
+    oj download https://atcoder.jp/contests/tessoku-book/tasks/tessoku_book_$q
+else
+    echo http://$dir.contest.atcoder.jp/tasks/$dir"_"$q
+    oj download http://$dir.contest.atcoder.jp/tasks/$dir"_"$q
+fi
+
 cp test/sample-1.in input
