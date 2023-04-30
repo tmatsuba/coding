8712e091d6ce582aeb6ee8453bca3193ea1e08ba
tmatsuba <tma@gside.org>
2023-04-30 17:29:29 +0900

    tessoku_book cl

---
+++ _result/_tessoku_book/cl/main.go
@@ -0,0 +1,118 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"math/bits"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	buf := make([]byte, 1024*1024)
+	sc.Buffer(buf, bufio.MaxScanTokenSize)
+	sc.Split(bufio.ScanWords)
+
+	N,K := nextInt(),nextInt()
+	A,S := make([]int, N), make([]int, N+1)
+	
+	for i:=0; i<N; i++ {
+		A[i] = nextInt()
+		S[i+1] = S[i] + A[i]
+	}
+
+	r := 1
+	ret := 0
+	for i:=0; i<N; i++ {
+		ret += r - i - 1
+		for ;r<N+1; r++ {
+			d := S[r] - S[i]
+			if d <= K {
+//				fmt.Println("i: ", i, "r: ", r, "d: " , d, "ret: ", ret)
+				ret++
+			} else {
+				break
+			}
+		}
+	}
+
+
+	fmt.Println(ret)
+}
+
+
+var sc = bufio.NewScanner(os.Stdin)
+
+func nextLine() string {
+    sc.Scan()
+    return sc.Text()
+}
+
+func nextInt() int {
+    sc.Scan()
+    i, e := strconv.Atoi(sc.Text())
+    if e != nil {
+        panic(e)
+    }
+    return i
+}
+
+func Max(x, y int) int {
+    if x < y {
+        return y
+    }
+    return x
+}
+
+func Min(x, y int) int {
+    if x > y {
+        return y
+    }
+    return x
+}
+
+func Abs(x int) int {
+	return int(math.Abs(float64(x)))
+}
+
+func atoi(s string) int {
+	ret, e := strconv.Atoi(s)
+	if e != nil {
+		panic(e)
+	}
+	return ret
+}
+
+func pow(a, b int) int {
+	return int(math.Pow(float64(a), float64(b)))
+}
+
+func combination(set []string, n int) (subsets [][]string) {
+	length := uint(len(set))
+
+	if n > len(set) {
+		n = len(set)
+	}
+
+	for subsetBits := 1; subsetBits < (1 << length); subsetBits++ {
+		if n > 0 && bits.OnesCount(uint(subsetBits)) != n {
+			continue
+		}
+
+		var subset []string
+
+		for object := uint(0); object < length; object++ {
+			if (subsetBits>>object)&1 == 1 {
+				subset = append(subset, set[object])
+			}
+		}
+		subsets = append(subsets, subset)
+	}
+	return subsets
+}
+
+func ArrayJoin(a []int, delim string) string {
+	return strings.Trim(strings.Replace(fmt.Sprint(a), " ", delim, -1), "[]")
+}
