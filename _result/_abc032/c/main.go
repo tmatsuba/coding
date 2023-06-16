ba7e5c2594d9fb3ba646ad71fb67070413cbb98a
tmatsuba <tma@gside.org>
2023-06-16 09:51:52 +0900

    abc032 c

---
+++ _result/_abc032/c/main.go
@@ -0,0 +1,136 @@
+package main
+
+import (
+	"os"
+	"bufio"
+	"fmt"
+	"strconv"
+	"math"
+	"math/bits"
+	"strings"
+)
+
+func main() {
+	buf := make([]byte, 1024*1024)
+	sc.Buffer(buf, bufio.MaxScanTokenSize)
+	sc.Split(bufio.ScanWords)
+
+	N,K  := nextInt(), nextInt()
+	S := make([]int, N)
+	ret := 0
+
+	for i:=0; i<N; i++ {
+		S[i] = nextInt()
+		if S[i] == 0 {
+			fmt.Println(N)
+			return
+		}
+	}
+
+	l:=0
+	r:=0
+	p := 1
+	for r < N {
+//		fmt.Println(p, S[r])
+		p = p * S[r]
+//		fmt.Println(l, r, p, ret)
+		if p <= K {
+			ret = Max(ret, r-l+1)
+			r++
+			continue 
+		} else {
+			for l < N {
+				p = p / S[l]
+				if p <= K {
+					l++
+					r++ 
+					break
+				} 
+
+				if r <= l {
+					l++
+					r = l
+					p = 1
+					break
+				}
+				l++
+			}
+		}
+	}
+	fmt.Println(ret)
+}
+
+
+var sc = bufio.NewScanner(os.Stdin)
+
+func nextLine() string {
+    sc.Scan()
+    return sc.Text()
+}
+
+func nextInt() int {
+    sc.Scan()
+    i, e := strconv.Atoi(sc.Text())
+    if e != nil {
+        panic(e)
+    }
+    return i
+}
+
+func Max(x, y int) int {
+    if x < y {
+        return y
+    }
+    return x
+}
+
+func Min(x, y int) int {
+    if x > y {
+        return y
+    }
+    return x
+}
+
+func Abs(x int) int {
+	return int(math.Abs(float64(x)))
+}
+
+func atoi(s string) int {
+	ret, e := strconv.Atoi(s)
+	if e != nil {
+		panic(e)
+	}
+	return ret
+}
+
+func pow(a, b int) int {
+	return int(math.Pow(float64(a), float64(b)))
+}
+
+func combination(set []string, n int) (subsets [][]string) {
+	length := uint(len(set))
+
+	if n > len(set) {
+		n = len(set)
+	}
+
+	for subsetBits := 1; subsetBits < (1 << length); subsetBits++ {
+		if n > 0 && bits.OnesCount(uint(subsetBits)) != n {
+			continue
+		}
+
+		var subset []string
+
+		for object := uint(0); object < length; object++ {
+			if (subsetBits>>object)&1 == 1 {
+				subset = append(subset, set[object])
+			}
+		}
+		subsets = append(subsets, subset)
+	}
+	return subsets
+}
+
+func ArrayJoin(a []int, delim string) string {
+	return strings.Trim(strings.Replace(fmt.Sprint(a), " ", delim, -1), "[]")
+}
