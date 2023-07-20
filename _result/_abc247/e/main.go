797692ef111428421948335facd43e1a3f9777aa
tmatsuba <tma@gside.org>
2023-07-20 09:41:20 +0900

    abc247 e

---
+++ _result/_abc247/e/main.go
@@ -0,0 +1,153 @@
+package main
+
+import (
+	"os"
+	"bufio"
+	"fmt"
+	"strconv"
+	"math"
+	"math/bits"
+	"strings"
+)
+
+func main() {
+	buf := make([]byte, 1024*1024)
+	sc.Buffer(buf, bufio.MaxScanTokenSize)
+	sc.Split(bufio.ScanWords)
+
+	N, X, Y := nextInt(), nextInt(), nextInt()
+	A := [][]int{}
+	ret := 0
+
+	j := 1
+
+
+
+	for i:=0; i<N; i++ {
+		v := nextInt()
+		if v <=X && v >= Y {
+			if len(A) < j {
+				A = append(A, []int{v})
+			} else {
+				A[j-1] = append(A[j-1], v)
+			}
+
+		} else {
+			if len(A) >= j {
+				j++
+			}
+		}
+	}
+
+	for _, a := range(A) {
+		min, max := 0, 0
+		r, l := 0, 0
+		for r<len(a) {
+			if a[r] == X {
+				max++
+			} 
+			if a[r] == Y {
+				min++
+			}
+			
+			if max > 0 && min > 0 {
+				for l <= r {
+					if max > 0 && min > 0 {
+						ret = ret + (len(a) - r) 
+					}
+					if a[l] == X {
+						max--
+					}
+					if a[l] == Y {
+						min--
+					}
+
+					l++
+
+					if max == 0 || min == 0 {
+						break
+					}
+
+				}
+			}
+			r++
+		}
+	}
+
+	fmt.Println(ret)
+}
+
+
+var sc = bufio.NewScanner(os.Stdin)
+
+func nextLine() string {
+    sc.Scan()
+    return sc.Text()
+}
+
+func nextInt() int {
+    sc.Scan()
+    i, e := strconv.Atoi(sc.Text())
+    if e != nil {
+        panic(e)
+    }
+    return i
+}
+
+func Max(x, y int) int {
+    if x < y {
+        return y
+    }
+    return x
+}
+
+func Min(x, y int) int {
+    if x > y {
+        return y
+    }
+    return x
+}
+
+func Abs(x int) int {
+	return int(math.Abs(float64(x)))
+}
+
+func atoi(s string) int {
+	ret, e := strconv.Atoi(s)
+	if e != nil {
+		panic(e)
+	}
+	return ret
+}
+
+func pow(a, b int) int {
+	return int(math.Pow(float64(a), float64(b)))
+}
+
+func combination(set []string, n int) (subsets [][]string) {
+	length := uint(len(set))
+
+	if n > len(set) {
+		n = len(set)
+	}
+
+	for subsetBits := 1; subsetBits < (1 << length); subsetBits++ {
+		if n > 0 && bits.OnesCount(uint(subsetBits)) != n {
+			continue
+		}
+
+		var subset []string
+
+		for object := uint(0); object < length; object++ {
+			if (subsetBits>>object)&1 == 1 {
+				subset = append(subset, set[object])
+			}
+		}
+		subsets = append(subsets, subset)
+	}
+	return subsets
+}
+
+func ArrayJoin(a []int, delim string) string {
+	return strings.Trim(strings.Replace(fmt.Sprint(a), " ", delim, -1), "[]")
+}
